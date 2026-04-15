baf1707c130601e2798c7503bf52df69df960c08
sandesh <dunbalesandesh2@gmail.com>
2026-04-15 18:18:00 +0530

    Initial Commit Added all Microservices,Gateway and eureka.

---
+++ ResourceAndInfrastructureService/src/main/java/com/example/demo/ResourceAndInfrastructureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAndInfrastructureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAndInfrastructureServiceApplication.class, args);
+	}
+
+}
