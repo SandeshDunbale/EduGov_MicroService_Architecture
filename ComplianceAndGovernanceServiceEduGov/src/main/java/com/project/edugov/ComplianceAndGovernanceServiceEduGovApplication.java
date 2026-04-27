544296261d6b88b7944faecfe760c7361dbc2f12
rohantade8 <rohantade8@gmail.com>
2026-04-27 14:30:04 +0530

    Module 6

--- ComplianceAndGovernanceServiceEduGov/src/main/java/com/project/edugov/ComplianceAndGovernanceServiceEduGovApplication.java
+++ ComplianceAndGovernanceServiceEduGov/src/main/java/com/project/edugov/ComplianceAndGovernanceServiceEduGovApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
 
 @SpringBootApplication 
 @EnableDiscoveryClient
@@ -14,5 +21,22 @@ public class ComplianceAndGovernanceServiceEduGovApplication {
         SpringApplication.run(
             ComplianceAndGovernanceServiceEduGovApplication.class, args
         );
+    }
+    
+    @Component
+    public class FeignClientInterceptor implements RequestInterceptor {
+        private static final String AUTHORIZATION_HEADER = "Authorization";
+
+        @Override
+        public void apply(RequestTemplate requestTemplate) {
+            ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (requestAttributes != null) {
+                HttpServletRequest request = requestAttributes.getRequest();
+                String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+                if (authHeader != null) {
+                    requestTemplate.header(AUTHORIZATION_HEADER, authHeader);
+                }
+            }
+        }
     }
 }
