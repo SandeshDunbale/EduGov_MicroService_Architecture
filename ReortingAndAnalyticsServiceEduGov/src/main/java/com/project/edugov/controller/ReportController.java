20fd682ee023d3a308e7c6758327613b626fec8d
mrunali <mrunalip263@gmail.com>
2026-05-11 15:14:31 +0530

    CHANGES for frontend

--- ReortingAndAnalyticsServiceEduGov/src/main/java/com/project/edugov/controller/ReportController.java
+++ ReortingAndAnalyticsServiceEduGov/src/main/java/com/project/edugov/controller/ReportController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/reports")
 @RequiredArgsConstructor
+
 public class ReportController {
  
     private final ReportService reportService;
